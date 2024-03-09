d8f1375333882148402cc7e9b6ee6b96c378a1ac
humansinc1 <humans.inc@gmail.com>
2024-03-09 20:49:03 +0300

    changed classes and styling

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 document.getElementById("joinus-button").addEventListener('click', () => {
     document.getElementById('welcome-form-name').focus();
 });
-document.getElementById("menu-button").addEventListener('click', () => {
+document.getElementById("menu-button-desktop").addEventListener('click', () => {
     document.getElementById('welcome-form-name').focus();
 });
 
@@ -13,11 +13,11 @@ window.onscroll = function () {scrollFunction()};
 function scrollFunction () {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("logo-link").style.display = "none";
-        document.getElementById('menu').style.margin = "0 auto";
-        document.getElementById('menu-items').style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        document.getElementById('menu-desktop').style.margin = "0 auto";
+        document.getElementById('menu-items-desktop').style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     } else {
         document.getElementById("logo-link").style.display = "block";
-        document.getElementById('menu').style.margin = "0";
-        document.getElementById('menu-items').style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+        document.getElementById('menu-desktop').style.margin = "0";
+        document.getElementById('menu-items-desktop').style.backgroundColor = "rgba(255, 255, 255, 0.1)";
     }
 };
